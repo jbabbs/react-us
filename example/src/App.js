4944eaea0b1168d925f806d7f33ffeeedfed4e0d
Justin Babbs <justin.babbs@gmail.com>
2019-07-26 01:45:38 -0400

    add a modal component

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
-
-import ExampleComponent from 'react-us'
+import { Modal } from 'react-us'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
-        <button class='usa-button usa-button--big'>USA-BUTTON</button>
+        <Modal />
       </div>
     )
   }
---
+++ src/modal/index.js
@@ -0,0 +1,15 @@
+/**
+ * @class ExampleComponent
+ */
+
+import React, { Component } from 'react'
+
+export class Modal extends Component {
+  render() {
+    return (
+      <div>
+        A Modal
+      </div>
+    )
+  }
+}
